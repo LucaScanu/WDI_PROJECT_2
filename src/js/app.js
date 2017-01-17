05667495627b869e53641210ffcc0c2acbb7c2f9
LucaScanu <lucagscanu@gmail.com>
2017-01-17 10:07:10 +0000

    updated for monday evening

--- src/js/app.js
+++ src/js/app.js
@@ -4,13 +4,11 @@ const google           = google;
 App.init               = function() {
   this.apiUrl          = 'http://localhost:3000/api';
   this.$main           = $('main');
-  $('#map').hide();
   $('.register').on('click', this.register.bind(this));
   $('.login').on('click', this.login.bind(this));
   $('.logout').on('click', this.logout.bind(this));
   $('.members').on('click', this.membersIndex.bind(this));
   $('main').on('submit', 'form', this.handleForm);
-
   if (this.getToken()) {
     this.loggedIn();
   } else {
@@ -22,17 +20,14 @@ App.loggedIn           = function() {
   $('.loggedIn').show();
   $('.loggedOut').hide();
   this.membersIndex();
-  $('#map').show();
-  App.initMap();
 };
 
 App.loggedOut          = function() {
   $('.loggedIn').hide();
   $('.loggedOut').show();
-  this.register();
 };
 
-App.surfInfo        = function(surf, marker) {
+App.surfInfo           = function(surf, marker) {
   google.maps.event.addListener(marker, 'click', () => {
     this.ajaxRequest(`http://localhost:3000/api/weather/${surf.lat}/${surf.lng}`, 'get', null, data => {
       $('.modal-body').html(`
@@ -59,7 +54,7 @@ App.surfInfo        = function(surf, marker) {
               The weather for today is ${data.summary}.
             </div>
             <div role="tabpanel" class="tab-pane" id="weather">
-              <iframe src="https://embed.windytv.com/?${surf.lat},${surf.lng},5,in:24,100m,waves,message,menu,marker,metric.wind.km/h" width="900" height="500" frameborder="0"></iframe>
+              <iframe src="https://embed.windytv.com/?${surf.lat},${surf.lng},5,in:24,100m,waves,message,menu,marker,metric.wind.km/h" width="900" height="600" frameborder="0"></iframe>
             </div>
             <div role="tabpanel" class="tab-pane" id="gallery">
 
@@ -108,73 +103,92 @@ App.surfInfo        = function(surf, marker) {
   });
 };
 
-App.marker          = function(surf) {
-  const latlng      = new google.maps.LatLng(surf.lat, surf.lng);
-  const marker      = new google.maps.Marker({
+App.marker             = function(surf) {
+  const latlng         = new google.maps.LatLng(surf.lat, surf.lng);
+  const marker         = new google.maps.Marker({
     position: latlng,
     map: App.map
   });
   App.surfInfo(surf, marker);
 };
 
-App.loopSurfs       = function(data) {
+App.loopSurfs          = function(data) {
   $.each(data.surfs, (index, surf) => {
     App.marker(surf);
   });
 };
 
-App.getSurfs        = function() {
+App.getSurfs           = function() {
   $.get('http://localhost:3000/api/surfs').done(this.loopSurfs);
 };
 
-App.initMap         = function() {
-  const map         = document.getElementById('map');
-  const mapOptions  = {
+App.initMap            = function() {
+  const map            = document.getElementById('map');
+  const mapOptions     = {
     center: new google.maps.LatLng(13.8302704,17.8480856),
     zoom: 2,
     mapTypeId: google.maps.MapTypeId.ROADMAP,
     styles: [{'featureType': 'administrative','elementType': 'labels.text.fill','stylers': [{'color': '#444444'}]},{'featureType': 'landscape','elementType': 'all','stylers': [{'color': '#f2f2f2'}]},{'featureType': 'poi','elementType': 'all','stylers': [{'visibility': 'off'}]},{'featureType': 'road','elementType': 'all','stylers': [{'saturation': -100},{'lightness': 45}]},{'featureType': 'road.highway','elementType': 'all','stylers': [{'visibility': 'simplified'}]},{'featureType': 'road.arterial','elementType': 'labels.icon','stylers': [{'visibility': 'off'}]},{'featureType': 'transit','elementType': 'all','stylers': [{'visibility': 'off'}]},{'featureType': 'water','elementType': 'all','stylers': [{'color': '#46bcec'},{'visibility': 'on'}]}]
   };
-  this.map          = new google.maps.Map(map, mapOptions);
+  this.map             = new google.maps.Map(map, mapOptions);
   this.getSurfs();
 };
 
 App.register           = function(e) {
   if(e) e.preventDefault();
 
-  this.$main.html(`
-    <h2>Register</h2>
-  <form method="post" action="/register">
-    <div class="form-group">
-      <input class="form-control" type="text" name="user[username]" placeholder="Username">
-    </div>
-    <div class="form-group">
-      <input class="form-control" type="email" name="user[email]" placeholder="Email">
-    </div>
-    <div class="form-group">
-      <input class="form-control" type="password" name="user[password]" placeholder="Password">
-    </div>
-    <div class="form-group">
-      <input class="form-control" type="password" name="user[passwordConfirmation]" placeholder="Password Confirmation">
-    </div>
-    <input class="btn btn-primary" type="submit" value="Register">
-  </form>`);
+  this.$main.html(`<div class='modal fade' tabindex='-1' role='dialog'>
+  <div class='modal-dialog' role='document'>
+  <div class='modal-content'>
+  <div class='modal-body'>
+  <h2>Register</h2>
+  <form method='post' action='/register'>
+  <div class='form-group'>
+  <input class='form-control' type='text' name='user[username]' placeholder='Username'>
+  </div>
+  <div class='form-group'>
+  <input class='form-control' type='email' name='user[email]' placeholder='Email'>
+  </div>
+  <div class='form-group'>
+  <input class='form-control' type='password' name='user[password]' placeholder='Password'>
+  </div>
+  <div class='form-group'>
+  <input class='form-control' type='password' name='user[passwordConfirmation]' placeholder='Password Confirmation'>
+  </div>
+  <input class='btn btn-primary' type='submit' value='Register'>
+  </form>
+  </div>
+  </div>
+  </div>
+  </div>
+  `);
+  $('.modal').modal('show');
 };
+
 
 App.login              = function(e) {
   if(e) e.preventDefault();
   this.$main.html(`
+    <div class='modal fade' tabindex='-1' role='dialog'>
+    <div class='modal-dialog' role='document'>
+    <div class='modal-content'>
+    <div class='modal-body'>
     <h2>Login</h2>
-    <form method="post" action="/login">
-      <div class="form-group">
-        <input class="form-control" type="email" name="email" placeholder="Email">
-      </div>
-      <div class="form-group">
-        <input class="form-control" type="password" name="password" placeholder="Password">
-      </div>
-      <input class="btn btn-primary" type="submit" value="Login">
+    <form method='post' action='/login'>
+    <div class='form-group'>
+    <input class='form-control' type='email' name='email' placeholder='Email'>
+    </div>
+    <div class='form-group'>
+    <input class='form-control' type='password' name='password' placeholder='Password'>
+    </div>
+    <input class='btn btn-primary' type='submit' value='Login'>
     </form>
-  `);
+    </div>
+    </div>
+    </div>
+    </div>
+    `);
+  $('.modal').modal('show');
 };
 
 App.logout = function(e) {
